46337a887aca0a8a41508e691c9a5a60e6bf0437
Timi <alabitimi65@gmail.com>
2022-07-26 00:41:27 -0600

    Updated state to focus on individual box

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -30,10 +30,18 @@ const data = [
 class Dashboard extends Component {
   state = {
     loading: false,
+    focused: null,
   };
+  selectPanel(id) {
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
+    }));
+  }
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
 
     if (this.state.loading) {
       return <Loading />;
@@ -49,6 +57,7 @@ class Dashboard extends Component {
         id={panel.id}
         label={panel.label}
         value={panel.value}
+        onSelect={(event) => this.selectPanel(panel.id)}
       />
     ));
 
